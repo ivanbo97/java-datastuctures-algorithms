cfa3dd8c2675175f7a0fd8a3fc7599d09c370c59
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-09-05 15:22:14 +0300

    Class for testing the implementation of chaining mechanism.

---
+++ src/com/ivanboyukliev/sortalgorithms/hashtables/arrayimpl/ChainedHashTableMain.java
@@ -0,0 +1,25 @@
+package com.ivanboyukliev.sortalgorithms.hashtables.arrayimpl;
+
+import com.ivanboyukliev.sortalgorithms.lists.arraylist.Employee;
+
+public class ChainedHashTableMain {
+    public static void main(String[] args) {
+        ChainedHashTable hashTable = new ChainedHashTable();
+        hashTable.put("Jones",new Employee("Jane","Jones",55));
+        hashTable.put("Poppins",new Employee("Mary","Poppins",95));
+        hashTable.put("Hardy",new Employee("Matt","Hardy",85));
+        hashTable.put("Scholes",new Employee("Paul","Scholes",25));
+        hashTable.put("Miller",new Employee("Foster","Miller",545));
+        hashTable.put("Wilson",new Employee("David","Wilson",455));
+        hashTable.put("End",new Employee("Bill","End",54));
+
+        //hashTable.printHashTable();
+
+       // System.out.println("Retrieve key Wilson: " + hashTable.get("Wilson"));
+
+        hashTable.removeEmployee("Poppins");
+        hashTable.removeEmployee("End");
+
+        hashTable.printHashTable();
+    }
+}
