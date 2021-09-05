743c5580721c56d0b916a2356d5e68fbf1566dff
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-09-05 14:22:18 +0300

    Introducing rehashing after the occurrence of collision.

--- src/com/ivanboyukliev/sortalgorithms/hashtables/arrayimpl/ArrayHashTable.java
+++ src/com/ivanboyukliev/sortalgorithms/hashtables/arrayimpl/ArrayHashTable.java
@@ -3,33 +3,100 @@ package com.ivanboyukliev.sortalgorithms.hashtables.arrayimpl;
 import com.ivanboyukliev.sortalgorithms.lists.arraylist.Employee;
 
 public class ArrayHashTable {
-    private Employee[] hashtable;
+    private StoredEmployee[] hashTable;
     private static final int HASHTABLE_CAPACITY = 10;
 
     public ArrayHashTable() {
-        hashtable = new Employee[HASHTABLE_CAPACITY];
+        hashTable = new StoredEmployee[HASHTABLE_CAPACITY];
     }
 
     private int hashKey(String key) {
-        return key.length() % hashtable.length;
+        return key.length() % hashTable.length;
     }
 
     public void put(String key, Employee value) {
         int hashedKey = hashKey(key);
-        if (hashtable[hashedKey] == null) {
-            hashtable[hashedKey] = value;
+        if (isPositionOccupied(hashedKey)) {
+            // Linear Probing
+            int stopIdx = hashedKey;
+            if (hashedKey == hashTable.length - 1) {
+                hashedKey = 0;
+            } else {
+                hashedKey++;
+            }
+            while (isPositionOccupied(hashedKey) && hashedKey != stopIdx) {
+                hashedKey = (hashedKey + 1) % hashTable.length;
+            }
+        }
+        if (hashTable[hashedKey] == null) {
+            hashTable[hashedKey] = new StoredEmployee(key, value);
             return;
         }
         System.out.println("There is already an employee at position: " + hashedKey);
     }
 
     public Employee get(String key) {
-        return hashtable[hashKey(key)];
+        int hashedKey = findKey(key);
+        if (hashedKey == -1) {
+            return null;
+        }
+        return hashTable[hashedKey].employee;
     }
 
-    public void printHashTable(){
-        for(Employee employee : hashtable){
-            System.out.println(employee);
+    public Employee remove(String key) {
+        int hashedKey = findKey(key);
+        if (hashedKey == -1) {
+            return null;
         }
+        Employee employee = hashTable[hashedKey].employee;
+        hashTable[hashedKey] = null;
+        // Rehashing
+        StoredEmployee[] oldHashtable = hashTable;
+        hashTable = new StoredEmployee[hashTable.length];
+        for (int i = 0; i < oldHashtable.length; i++) {
+            if (oldHashtable[i] != null) {
+                put(oldHashtable[i].key, oldHashtable[i].employee);
+            }
+        }
+        return employee;
+    }
+
+    private int findKey(String rawKey) {
+        int hashedKey = hashKey(rawKey);
+        if (hashTable[hashedKey] != null && hashTable[hashedKey].key.equals(rawKey)) {
+            return hashedKey;
+        }
+
+        int stopIdx = hashedKey;
+        if (hashedKey == hashTable.length - 1) {
+            hashedKey = 0;
+        } else {
+            hashedKey++;
+        }
+
+        while (hashedKey != stopIdx &&
+                hashTable[hashedKey] != null
+                && !hashTable[hashedKey].equals(rawKey)) {
+            hashedKey = (hashedKey + 1) % hashTable.length;
+        }
+
+        if (hashTable[hashedKey] != null && hashTable[hashedKey].key.equals(rawKey)) {
+            return hashedKey;
+        }
+        return -1;
+    }
+
+    public void printHashTable() {
+        for (StoredEmployee storedEmployee : hashTable) {
+            if (storedEmployee == null) {
+                System.out.println("empty");
+            } else {
+                System.out.println(storedEmployee.employee);
+            }
+        }
+    }
+
+    private boolean isPositionOccupied(int index) {
+        return hashTable[index] != null;
     }
 }
