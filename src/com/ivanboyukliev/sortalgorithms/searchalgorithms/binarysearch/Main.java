778d68190b59ab196a2a1e8e428f7e7cdfcaa54c
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-09-06 14:16:27 +0300

    Implementing binary search recursively.

--- src/com/ivanboyukliev/sortalgorithms/searchalgorithms/binarysearch/Main.java
+++ src/com/ivanboyukliev/sortalgorithms/searchalgorithms/binarysearch/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {-22, -15, 1, 7, 20, 35, 55};
         System.out.println(iterativeBinarySearch(array, 7));
+
+        System.out.println(recursiveBinarySearch(array, 0, array.length, 35));
     }
 
     public static int iterativeBinarySearch(int[] array, int searchedVal) {
@@ -24,5 +26,17 @@ public class Main {
             end = midPoint;
         }
         return -1;
+    }
+
+
+    public static int recursiveBinarySearch(int[] array, int start, int end, int searchedVal) {
+        if (start >= end) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (array[mid] == searchedVal) {
+            return mid;
+        }
+        return searchedVal > array[mid] ? recursiveBinarySearch(array, mid + 1, end, searchedVal) : recursiveBinarySearch(array, start, mid, searchedVal);
     }
 }
