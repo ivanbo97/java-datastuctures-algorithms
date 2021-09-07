7a719e517569710551da3adf0ebc277cb2d901b2
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-09-07 14:59:37 +0300

    Class for testing heap related operations.

---
+++ src/com/ivanboyukliev/heap/Main.java
@@ -0,0 +1,20 @@
+package com.ivanboyukliev.heap;
+
+public class Main {
+    public static void main(String[] args) {
+        Heap heap = new Heap(10);
+
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        heap.delete(0);
+        heap.printHeap();
+
+    }
+}
